b766e7a5727488c9318623ff8e05b8acb7934b85
“ola <waleabi764@gmail.com>
2024-06-18 10:29:47 -0400

    creating new project

---
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.noteapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.noteapp.ui.theme.NoteAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    NoteAppTheme {
+        content()
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NoteAppTheme {
+        MyApp {
+
+        }
+    }
+}
